03f18b825160eb3a24633a517dc20e14b37c0fbb
sajivddin <sajivddin.websolutions@gmail.com>
2025-07-19 17:19:53 +0530

    change

--- lib/screen/security.page.dart
+++ lib/screen/security.page.dart
@@ -1,10 +1,9 @@
 import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:k2_app/screen/notification.page.dart';
+
 
 class SecurityPage extends StatefulWidget {
   const SecurityPage({super.key});
